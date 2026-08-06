f8f0b41003734713f8a5ab4098d5bbd4b54191df
bandwstudio2025 <bandwstudio2025@gmail.com>
2026-08-05 21:09:44 -0400

    Fear System done
    Added the fear system
    fear goes up when light is of
    fear goes down when light in on

---
+++ Assets/Scripts/FearSystem.cs
@@ -0,0 +1,82 @@
+using System.ComponentModel.Design;
+using Unity.VisualScripting;
+using UnityEngine;
+using TMPro;
+using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
+
+public class FearSystem : MonoBehaviour
+{
+
+    [Header("Fear Sysstem")]
+    [Range(1, 100)][SerializeField] float maxFear;
+    [SerializeField] float currentFear;
+    [SerializeField] float fearAccumulationRate;
+    [SerializeField] float calmRate;
+
+    [Header("UI Reference")]
+    [SerializeField] TextMeshProUGUI fearText;
+    [SerializeField] GameObject gameOverPanel;
+    [SerializeField] TextMeshProUGUI gameOverMessageText;
+
+
+    [Header("Reference")]
+    [SerializeField] FlashlightController flashlightController;
+
+    [Header("State")]
+    public bool isInsane = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        currentFear = 0f;
+        isInsane = false;
+        UpdateFearUI();
+        if(gameOverPanel != null) gameOverPanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isInsane) return;
+
+        Light playerLight = flashlightController != null ? flashlightController.GetComponentInChildren<Light>() : null;
+        bool isLightActive = playerLight != null && playerLight.enabled;
+
+        if (isLightActive) DecreaseFear(calmRate * Time.deltaTime);
+        else IncreaseFear(fearAccumulationRate * Time.deltaTime);
+
+        if (currentFear >= maxFear && !isInsane) TriggerPanicState();
+
+        // Check if player has completely lost sanity
+        if (currentFear >= maxFear && !isInsane) TriggerPanicState();
+    }
+    void IncreaseFear(float amount)
+    {
+        currentFear += amount;
+        currentFear = Mathf.Clamp(currentFear, 0f, maxFear);
+        UpdateFearUI();
+    }
+    void DecreaseFear(float amount)
+    {
+        currentFear -= amount;
+        currentFear = Mathf.Clamp(currentFear, 0f, maxFear);
+        UpdateFearUI();
+    }
+    void UpdateFearUI()
+    {
+        if (fearText != null) fearText.text = "Fear: " + Mathf.Round(currentFear) + "%";
+    }
+    void TriggerPanicState()
+    {
+        isInsane = true;
+        Debug.Log("Player has reached maximun psycholgical fear/insanity!");
+
+        // Change the game over message text
+        if (gameOverMessageText != null) gameOverMessageText.text = "You went insane!";
+
+        if(gameOverPanel != null) gameOverPanel.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+}
