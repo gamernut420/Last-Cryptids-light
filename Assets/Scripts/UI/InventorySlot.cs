767bd0be127d943777ce080ed2270d318b2d4774
Shawn Evelo <scardydragon49@gmail.com>
2026-09-02 18:36:40 -0600

    Created UI for hotbar

---
+++ Assets/Scripts/UI/InventorySlot.cs
@@ -0,0 +1,46 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventorySlot : MonoBehaviour
+{
+    [SerializeField] Image itemImage;
+    [SerializeField] TextMeshProUGUI itemText;
+    [SerializeField] TextMeshProUGUI itemCount;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        itemImage.enabled = false;
+        itemText.enabled = false;
+        itemCount.enabled = false;
+    }
+
+    public void UpdateSlot(Sprite _itemImage, string _itemText, int _itemCount)
+    {
+        if(_itemImage != null)
+        {
+            itemImage.enabled = true;
+            itemText.enabled = false;
+
+            itemImage.sprite = _itemImage;
+        }
+        else
+        {
+            itemText.enabled = true;
+            itemImage.enabled = false;
+
+            itemText.text = _itemText;
+        }
+
+        if(_itemCount > 1)
+        {
+            itemCount.enabled = true;
+            itemCount.text = _itemCount.ToString();
+        }
+        else
+        {
+            itemCount.enabled = false;
+        }
+    }
+}
