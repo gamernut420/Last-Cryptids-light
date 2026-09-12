ec5305f793df929d9a05b334dd98557ae35acf88
Alan <bandwstudio2025@gmail.com>
2026-09-12 16:32:17 -0400

    Objectives Done

--- Assets/Scripts/Objectives/ObjectiveManager.cs
+++ Assets/Scripts/Objectives/ObjectiveManager.cs
@@ -7,6 +7,7 @@ public class ObjectiveManager : MonoBehaviour
 
     [Header("Active Objectives")]
     public List<ObjectiveData> activeObjectives = new List<ObjectiveData>();
+    public int hideUIin;
 
     public Transform currentCompassTarget;
 
@@ -19,6 +20,21 @@ public class ObjectiveManager : MonoBehaviour
             return;
         }
         Instance = this;
+        foreach (var obj in activeObjectives)
+        {
+            if (obj != null)
+            {
+                obj.shouldHideFromUI = false;
+                obj.isCompleted = false;
+                // if it has no prerequisite, unlock it immadiately. Otherwise, lock it.
+                if (obj.prerequisiteObjective == null)
+                    obj.isUnlocked = true;
+                else
+                    obj.isUnlocked = false;
+
+
+            }
+        }
     }
 
     public void AddObjective(ObjectiveData newObj)
@@ -36,13 +52,36 @@ public class ObjectiveManager : MonoBehaviour
     public void CompleteObjective(string objectiveID)
     {
         ObjectiveData targetObjective = activeObjectives.Find(o => o.objectiveID == objectiveID);
-        if (targetObjective != null && !targetObjective.isCompleted)
+        if (targetObjective != null && targetObjective.isUnlocked && !targetObjective.isCompleted)
         {
             targetObjective.isCompleted = true;
             Debug.Log($"Objective Completed: {targetObjective.objectiveTitle}");
 
-            // Remove from active list or transition to next objective here
+            // Call this right here to unlock any objectives waiting on this one
+            UnlockNextObjective(targetObjective);
+            // Wait x seconds, turn hide it from the UI
+            StartCoroutine(HideObjectiveRoutine(targetObjective, hideUIin));
+        }
+    }
 
+    private System.Collections.IEnumerator HideObjectiveRoutine(ObjectiveData objectiveToHide, float delay)
+    {
+        Debug.Log($"Objective hide in 2 secs: {objectiveToHide.objectiveTitle}");
+
+        yield return new WaitForSeconds(delay);
+        objectiveToHide.shouldHideFromUI = true;
+    }
+    private void UnlockNextObjective(ObjectiveData completedObjective)
+    {
+        // Loop through your active objectives (or a master list of all game objectives)
+        foreach(var obj in activeObjectives)
+        {
+            if(!obj.isUnlocked && obj.prerequisiteObjective == completedObjective)
+            {
+                obj.isUnlocked = true;
+                Debug.Log($"Objective Unlocked: { obj.objectiveTitle}");
+
+            }
         }
     }
 }
