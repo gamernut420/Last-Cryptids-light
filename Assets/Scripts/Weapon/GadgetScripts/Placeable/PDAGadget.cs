b6a617059a17b435fbac8e6251ab96cffbea497e
Shawn Evelo <scardydragon49@gmail.com>
2026-09-16 10:58:52 -0600

    Added PDA

--- Assets/Scripts/Weapon/GadgetScripts/Placeable/PDAGadget.cs
+++ Assets/Scripts/Weapon/GadgetScripts/Placeable/PDAGadget.cs
@@ -2,25 +2,38 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class FlareThrowable : ThrowableGadget
+public class PDAGadget : PlaceableGadget
 {
     [SerializeField][Min(0)] float EffectDuration = 2.5f;
     [SerializeField][Min(0)] float EffectSize = 10;
 
-    public override void Impact(RaycastHit hit)
+    public override bool UseGadget(GameObject player)
     {
-        transform.position = hit.point;
+        if (PlaceGadget())
+        {
+            StartCoroutine(CreateSafeZone());
 
-        StartCoroutine(CreateSafeZone());
+            return true;
+        }
+
+        return false;
     }
 
     IEnumerator CreateSafeZone()
     {
+        gameObject.AddComponent<SafeZoneTrigger>();
+
         NavMeshObstacle obstacle = gameObject.AddComponent<NavMeshObstacle>();
 
         obstacle.shape = NavMeshObstacleShape.Capsule;
 
         obstacle.radius = EffectSize;
+
+        SphereCollider safeZone = gameObject.AddComponent<SphereCollider>();
+
+        safeZone.isTrigger = true;
+
+        safeZone.radius = EffectSize;
 
         yield return new WaitForSeconds(EffectDuration);
 
