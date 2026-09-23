951cb61a431f765ff950ad0a39682a9fdb84da6c
Shawn Evelo <scardydragon49@gmail.com>
2026-09-22 19:46:05 -0600

    Stamina bar bug fixes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -422,7 +422,7 @@ public class gameManager : MonoBehaviour
 
     public void UpdateStaminaBar(float ammount, bool show)
     {
-        staminaUI.SetActive(show);
+        //staminaUI.SetActive(show);
 
         staminaBar.fillAmount = ammount;
     }
